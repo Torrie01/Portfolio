b6a22c95713341e61aec468d38e4937409ec0a88
Torrie01 <t.asifor@yahoo.com>
2023-09-26 16:19:47 +0100

    My Portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -89,7 +89,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/Torrie01/portfolio"
+                href="https://github.com/Torrie01/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
